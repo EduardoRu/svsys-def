244e8ff946d2fbd2c562793e59dcf2404b4efc20
EduardoRu <91968836+EduardoRu@users.noreply.github.com>
2024-12-02 22:27:05 -0600

    RP plantilla

--- src/app/components/add-car/add-car.component.ts
+++ src/app/components/add-car/add-car.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { IonicModule, ModalController } from '@ionic/angular';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AlertController, IonicModule, LoadingController, ModalController, ToastController } from '@ionic/angular';
+import { AddCarService } from 'src/app/services/actividades/addCar/add-car.service';
 
 @Component({
   selector: 'app-add-car',
@@ -12,14 +13,48 @@ import { IonicModule, ModalController } from '@ionic/angular';
 })
 export class AddCarComponent  implements OnInit {
 
+  public auto: FormGroup
+
   constructor(
-    private modalController: ModalController
+    private modalController: ModalController,
+    private fb: FormBuilder,
+    private addCarService: AddCarService,
+    private toastController: ToastController,
+    private loadingController: LoadingController
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.auto = this.fb.group({
+      placa: ['', Validators.required]
+    });
+  }
+
+  async presentToast(msg: string, position: 'top' | 'middle' | 'bottom', color: "danger" | "dark" | "light" | "success" | "warning") {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 1500,
+      position: position,
+      color: color
+    });
+
+    await toast.present();
+  }
+
 
   async cancel(){
     await this.modalController.dismiss();
+  }
+
+  async guardarAuto(){
+    if(this.auto.valid){
+      this.addCarService.addCar(this.auto.value).then(() => {
+        this.modalController.dismiss();
+        this.auto.reset();
+        this.presentToast("Auto agregado exitosamente!", "bottom", "success");
+      })
+    }else{
+      this.presentToast("Favor de completar todos los campos!", "bottom", "warning");
+    }
   }
 
 }
--- src/app/pages/vehi-equi/vehi-equi.page.ts
+++ src/app/pages/vehi-equi/vehi-equi.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AlertController, ModalController } from '@ionic/angular';
 import { AddCarComponent } from 'src/app/components/add-car/add-car.component';
 import { AddToolComponent } from 'src/app/components/add-tool/add-tool.component';
+import { AddCarService } from 'src/app/services/actividades/addCar/add-car.service';
 
 @Component({
   selector: 'app-vehi-equi',
@@ -10,17 +11,33 @@ import { AddToolComponent } from 'src/app/components/add-tool/add-tool.component
 })
 export class VehiEquiPage implements OnInit {
 
+  public autos:any = []
+
   constructor(
     private modalController: ModalController,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private carService: AddCarService
   ) { }
 
   ngOnInit() {
+    this.getInformacion();
+  }
+
+  async getInformacion(){
+    this.carService.getCar().subscribe({
+      next: (car) => {
+        this.autos = car
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    })
   }
 
   async addNewCar(){
     const modalCar = await this.modalController.create({
-      component: AddCarComponent
+      component: AddCarComponent,
+      cssClass: 'modalAutos'
     });
 
     modalCar.present();
@@ -32,6 +49,7 @@ export class VehiEquiPage implements OnInit {
     });
 
     modalTool.present();
+
   }
 
 }
